60e3e17d2ecb68c954f97c97e543e3196acd5165
Magma4 <youngmagma@protonmail.com>
2024-12-01 21:57:10 +0000

    Added order by

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -9,7 +9,8 @@ ON Booking.user_id = User.user_id;
 SELECT *
 FROM Property
 LEFT JOIN Review 
-ON Property.property_id = Review.property_id;
+ON Property.property_id = Review.property_id
+ORDER BY Review.rating DESC;
 
 
 -- Retrieve All Users and All Bookings (Full Outer Join)

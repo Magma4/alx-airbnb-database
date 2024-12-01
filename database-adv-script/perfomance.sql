3e3616a2f6f0729c559a21757c6f0c7c29aa3cda
Magma4 <youngmagma@protonmail.com>
2024-12-01 22:03:31 +0000

    Added where and AND

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -12,4 +12,29 @@ ON
 LEFT JOIN 
     Payment 
 ON 
-    Booking.booking_id = Payment.booking_id;
+    Booking.booking_id = Payment.booking_id
+WHERE 
+    Booking.status = 'confirmed'
+    AND Booking.start_date BETWEEN '2024-01-01' AND '2024-03-31';
+
+
+
+-- Analyze the query’s performance using EXPLAIN and identify any inefficiencies.
+EXPLAIN SELECT *
+FROM 
+    Booking
+JOIN 
+    User 
+ON 
+    Booking.user_id = User.user_id
+JOIN 
+    Property 
+ON 
+    Booking.property_id = Property.property_id
+LEFT JOIN 
+    Payment 
+ON 
+    Booking.booking_id = Payment.booking_id
+WHERE 
+    Booking.status = 'confirmed'
+    AND Booking.start_date BETWEEN '2024-01-01' AND '2024-03-31';
